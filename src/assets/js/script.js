a32d136b052925bda4a10eb2f7d7e6614bef50a9
Andrei Artuhanau <andrei-artuhanau@expert-soft.net>
2026-03-12 20:19:05 +0300

    Fix issues 1,3,4,5,6, 7, 9,10,11,13,14,15,16,17,18,19

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,30 +1,12 @@
 const ID = "id";
-const LIST_ITEM_TO_SET = {
-  HIDDEN: "product-list-item__action--hidden",
-  FAVORITES: "product-list-item__action--favorite",
-  COMPARE: "product-list-item__action--compare",
-};
-
-const HIDDEN_SET = new Set(
-  JSON.parse(localStorage.getItem(LIST_ITEM_TO_SET.HIDDEN) ?? "[]"),
-);
-const FAVORITES_SET = new Set(
-  JSON.parse(localStorage.getItem(LIST_ITEM_TO_SET.FAVORITES) ?? "[]"),
-);
-const COMPARE_SET = new Set(
-  JSON.parse(localStorage.getItem(LIST_ITEM_TO_SET.COMPARE) ?? "[]"),
-);
-
-const SET_TO_LIST_ITEM_TAG = new Map([
-  [COMPARE_SET, "product-list-item__action--compare"],
-  [FAVORITES_SET, "product-list-item__action--favorite"],
-  [HIDDEN_SET, "product-list-item__action--hidden"],
-]);
 
 const CLASSES = {
+  ACTIONS: "product-list-item__actions",
   ACTION: "product-list-item__action",
   ACTION_ACTIVE: "product-list-item__action--active",
   ACTION_HIDDEN: "product-list-item__action--hidden",
+  ACTION_COMPARE: "product-list-item__action--compare",
+  ACTION_FAVORITE: "product-list-item__action--favorite",
   MENU_COMPARE: "product-grid__menu-item--compare",
   MENU_ALL: "product-grid__menu-item--all",
   MENU_FAVORITES: "product-grid__menu-item--favorites",
@@ -37,76 +19,108 @@ const CLASSES = {
   LIST_ITEM: "product-list-item",
 };
 
-const ACTION_SET = new Set([
-  CLASSES.MENU_ALL,
-  CLASSES.MENU_COMPARE,
-  CLASSES.MENU_FAVORITES,
-]);
-let isHiddenListItemVisible = document.getElementById("hidden").checked;
+const HIDDEN_SET = new Set(
+  JSON.parse(localStorage.getItem(CLASSES.ACTION_HIDDEN) ?? "[]"),
+);
+const FAVORITES_SET = new Set(
+  JSON.parse(localStorage.getItem(CLASSES.ACTION_FAVORITE) ?? "[]"),
+);
+const COMPARE_SET = new Set(
+  JSON.parse(localStorage.getItem(CLASSES.ACTION_COMPARE) ?? "[]"),
+);
 
-window.addEventListener("DOMContentLoaded", (event) => {
+const LIST_ITEM_TO_SET = new Map([
+  [CLASSES.ACTION_COMPARE, COMPARE_SET],
+  [CLASSES.ACTION_FAVORITE, FAVORITES_SET],
+  [CLASSES.ACTION_HIDDEN, HIDDEN_SET],
+]);
+
+const SET_TO_LIST_ITEM_TAG = new Map([
+  [COMPARE_SET, CLASSES.ACTION_COMPARE],
+  [FAVORITES_SET, CLASSES.ACTION_FAVORITE],
+  [HIDDEN_SET, CLASSES.ACTION_HIDDEN],
+]);
+
+const SELECTOR_MENU_ITEMS = document.querySelectorAll(`.${CLASSES.MENU_ITEM}`);
+const SELECTOR_LIST_ITEMS = document.querySelectorAll(`.${CLASSES.LIST_ITEM}`);
+const SELECTOR_ALL_ACTIONS = document.querySelectorAll(`.${CLASSES.ACTIONS}`);
+
+let isHiddenListItemVisible = document.getElementById("hidden").checked;
+let activateMenuItem = CLASSES.MENU_ALL;
+
+window.addEventListener("DOMContentLoaded", () => {
   initializeActionButtons();
   initializeMenu();
   initializeListItems();
 });
 
 function initializeActionButtons() {
-  document.querySelectorAll(`.${CLASSES.ACTION}`).forEach((element) => {
-    element.removeEventListener("click", handleActionButtonClick);
+  SELECTOR_ALL_ACTIONS.forEach((element) => {
     element.addEventListener("click", handleActionButtonClick);
   });
 }
 
 function handleActionButtonClick(event) {
-  const action = event.currentTarget.closest(`.${CLASSES.ACTION}`);
-  const listItem = event.currentTarget.closest(`.${CLASSES.LIST_ITEM}`);
-  if (action.classList.contains(CLASSES.ACTION_ACTIVE)) {
-    deactivateButton(listItem, action);
+  const action = event.target.closest(`.${CLASSES.ACTION}`);
+  const listItem = event.target.closest(`.${CLASSES.LIST_ITEM}`);
+  let currentActionName = getActionName(action);
+  action.classList.toggle(CLASSES.ACTION_ACTIVE);
+  let isActive = action.classList.contains(CLASSES.ACTION_ACTIVE);
+
+  updateSetValues(listItem, currentActionName, isActive);
+
+  changeCurrentItemVisibility(listItem, currentActionName, isActive);
+}
+
+function updateSetValues(listItem, currentActionName, isActive) {
+  if (isActive) {
+    LIST_ITEM_TO_SET.get(currentActionName).add(listItem.getAttribute(ID));
   } else {
-    activeButton(listItem, action);
+    LIST_ITEM_TO_SET.get(currentActionName).delete(listItem.getAttribute(ID));
   }
   saveSets();
 }
 
-function activeButton(listItem, action) {
-  action.classList.add(CLASSES.ACTION_ACTIVE);
-  SET_TO_LIST_ITEM_TAG.forEach((value, key) => {
-    if (action.classList.contains(value)) {
-      key.add(listItem.getAttribute(ID));
-    }
-  });
-
-  if (action.classList.contains(CLASSES.ACTION_HIDDEN)) {
-    listItem.classList.add(CLASSES.LIST_ITEM_TRANSPARENT);
-    if (!isHiddenListItemVisible) {
-      listItem.classList.add(CLASSES.LIST_ITEM_HIDDEN);
-    }
-  }
+function getActionName(action) {
+  return [...LIST_ITEM_TO_SET.keys()].find((key) =>
+    action.classList.contains(key),
+  );
 }
 
-function deactivateButton(listItem, action) {
-  action.classList.remove(CLASSES.ACTION_ACTIVE);
-  SET_TO_LIST_ITEM_TAG.forEach((value, key) => {
-    if (action.classList.contains(value)) {
-      key.delete(listItem.getAttribute(ID));
+function changeCurrentItemVisibility(listItem, actionName, isActive) {
+  if (isActive) {
+    if (actionName === CLASSES.ACTION_HIDDEN) {
+      listItem.classList.add(CLASSES.LIST_ITEM_TRANSPARENT);
+      if (!isHiddenListItemVisible) {
+        listItem.classList.add(CLASSES.LIST_ITEM_HIDDEN);
+      }
     }
-  });
-  if (action.classList.contains(CLASSES.ACTION_HIDDEN)) {
-    listItem.classList.remove(CLASSES.LIST_ITEM_TRANSPARENT);
+  } else {
+    if (
+      (actionName === CLASSES.ACTION_COMPARE &&
+        activateMenuItem === CLASSES.MENU_COMPARE) ||
+      (actionName === CLASSES.ACTION_FAVORITE &&
+        activateMenuItem === CLASSES.MENU_FAVORITES)
+    ) {
+      listItem.classList.add(CLASSES.LIST_ITEM_HIDDEN);
+    }
+    if (actionName === CLASSES.ACTION_HIDDEN) {
+      listItem.classList.remove(CLASSES.LIST_ITEM_TRANSPARENT);
+    }
   }
 }
 
 function saveSets() {
   localStorage.setItem(
-    LIST_ITEM_TO_SET.HIDDEN,
+    CLASSES.LIST_ITEM_HIDDEN,
     JSON.stringify([...HIDDEN_SET]),
   );
   localStorage.setItem(
-    LIST_ITEM_TO_SET.FAVORITES,
+    CLASSES.ACTION_FAVORITE,
     JSON.stringify([...FAVORITES_SET]),
   );
   localStorage.setItem(
-    LIST_ITEM_TO_SET.COMPARE,
+    CLASSES.ACTION_COMPARE,
     JSON.stringify([...COMPARE_SET]),
   );
 }
@@ -143,54 +157,31 @@ const handlers = {
 
 function handleChooser() {
   isHiddenListItemVisible = !isHiddenListItemVisible;
-  let element =
-    document.querySelector(
-      `.${CLASSES.MENU_ITEM_ACTIVE}.${CLASSES.MENU_ITEM}`,
-    ) ?? document.querySelector(`.${CLASSES.MENU_ALL}`);
-
-  let activeAction = [...element.classList].filter((className) =>
-    ACTION_SET.has(className),
-  );
-  if (activeAction.length == 1) {
-    handlers[activeAction[0]]();
-  }
+  handlers[activateMenuItem]();
 }
 
-function handleMenuItemClick(selector, predicate) {
-  activateMenuItem(document.querySelector(`.${selector}`));
-  changeListItemsVisibility(predicate);
-}
-
-function activateMenuItem(actionElement) {
-  if (actionElement != null) {
-    document.querySelectorAll(`.${CLASSES.MENU_ITEM}`).forEach((actions) => {
-      actions.classList.remove(CLASSES.MENU_ITEM_ACTIVE);
+function handleMenuItemClick(activeMenuItemClassName, predicate) {
+  activateMenuItem = activeMenuItemClassName;
+  (function activateMenuItem(activeMenuItemClassName) {
+    SELECTOR_MENU_ITEMS.forEach((menuItem) => {
+      menuItem.classList.remove(CLASSES.MENU_ITEM_ACTIVE);
+      if (menuItem.classList.contains(activeMenuItemClassName)) {
+        menuItem.classList.add(CLASSES.MENU_ITEM_ACTIVE);
+      }
     });
-    actionElement.classList.add(CLASSES.MENU_ITEM_ACTIVE);
-  }
-}
+  })(activeMenuItemClassName);
 
-function changeListItemsVisibility(predicate) {
-  document.querySelectorAll(`.${CLASSES.LIST_ITEM}`).forEach((element) => {
-    changeListItemVisibility(predicate, element);
-  });
-}
+  changeListItemsVisibility(predicate);
 
-function changeListItemVisibility(predicate, element) {
-  if (
-    predicate(element) ||
-    (!isHiddenListItemVisible && HIDDEN_SET.has(element.getAttribute(ID)))
-  ) {
-    element.classList.add(CLASSES.LIST_ITEM_HIDDEN);
-  } else {
-    element.classList.remove(CLASSES.LIST_ITEM_HIDDEN);
+  function changeListItemsVisibility(predicate) {
+    SELECTOR_LIST_ITEMS.forEach((element) => {
+      changeListItemVisibility(predicate, element);
+    });
   }
 }
 
 function initializeListItems() {
-  document
-    .querySelectorAll(`.${CLASSES.LIST_ITEM}`)
-    .forEach((listItem) => updateListItem(listItem));
+  SELECTOR_LIST_ITEMS.forEach((listItem) => updateListItem(listItem));
 }
 
 function updateListItem(listItem) {
@@ -204,4 +195,15 @@ function updateListItem(listItem) {
     }
   });
   changeListItemVisibility(() => false, listItem);
+}
+
+function changeListItemVisibility(predicate, element) {
+  if (
+    predicate(element) ||
+    (!isHiddenListItemVisible && HIDDEN_SET.has(element.getAttribute(ID)))
+  ) {
+    element.classList.add(CLASSES.LIST_ITEM_HIDDEN);
+  } else {
+    element.classList.remove(CLASSES.LIST_ITEM_HIDDEN);
+  }
 }
